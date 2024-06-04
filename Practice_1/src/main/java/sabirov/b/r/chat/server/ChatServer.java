66f4612b8dff02c21669d552be55ccf3b26a32eb
Bumbeesh <128692537+Bumbeesh@users.noreply.github.com>
2024-06-04 19:12:53 +0300

    added full application

---
+++ Practice_1/src/main/java/sabirov/b/r/chat/server/ChatServer.java
@@ -0,0 +1,11 @@
+package sabirov.b.r.chat.server;
+
+import java.io.IOException;
+
+public class ChatServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerListener serverListener = new ServerListener();
+        serverListener.start();
+    }
+}
